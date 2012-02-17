00f5cf328a0250373c213de496f6fbcf40956213
ianwookim <ianwookim@gmail.com>
2012-02-17 07:01:23 +0000

    chang parser to enumerator version, xournal-parser-0.4.1

--- lib/Application/XournalConvert/Job.hs
+++ lib/Application/XournalConvert/Job.hs
@@ -4,21 +4,24 @@ module Application.XournalConvert.Job where
 
 import System.Directory 
 import Data.Xournal.Simple
-import Text.Xournal.Parse
+-- import Text.Xournal.Parse
+import Text.Xournal.Parse.Enumerator 
 import Application.XournalConvert.Convert.MakeSVG
 
-
--- startJob :: IO () 
--- startJob = do 
---   putStrLn "job started"
-
+-- | 
 
 startMakeSVG :: FilePath -> Maybe FilePath -> IO () 
 startMakeSVG fname mdest = do 
-  xojcontent <- checkIfBinary fname >>= \b -> 
-                  ifThenElse b (read_xojgz fname) (read_xournal fname)
-  cdir <- getCurrentDirectory 
-  let dest = maybe cdir id mdest 
-  makeSVGFromXournal xojcontent fname dest 
-  return ()
+  exojcontent <- parseXournal fname 
+  case exojcontent of 
+    Left err -> putStrLn $ "parsing error : " ++ err 
+    Right xojcontent -> do 
+      cdir <- getCurrentDirectory 
+      let dest = maybe cdir id mdest 
+      makeSVGFromXournal xojcontent fname dest 
+      return ()
+
+
+  -- xojcontent <- checkIfBinary fname >>= \b -> 
+  --                 ifThenElse b (read_xojgz fname) (read_xournal fname)
 
